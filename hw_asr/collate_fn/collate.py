9821d782c155adc2f9bdce159887de73ef6ed890
ivan <ivangorin21@yandex.ru>
2021-10-14 16:22:58 +0300

    tests and beam_search

--- hw_asr/collate_fn/collate.py
+++ hw_asr/collate_fn/collate.py
@@ -1,6 +1,10 @@
 import logging
 from typing import List
 
+from torch.nn.utils.rnn import pad_sequence
+import torch
+from hw_asr.text_encoder.char_text_encoder import CharTextEncoder
+
 logger = logging.getLogger(__name__)
 
 
@@ -9,6 +13,24 @@ def collate_fn(dataset_items: List[dict]):
     Collate and pad fields in dataset items
     """
 
-    result_batch = {}
-    # TODO: your code here
-    raise NotImplementedError
+    result_batch = {'text_encoded': [],
+                    'text_encoded_length': [],
+                    'spectrogram': [],
+                    'spectrogram_length': [],
+                    'text': [],
+                    }
+
+    for item in dataset_items:
+        result_batch['text_encoded'].append(item['text_encoded'].squeeze())
+        result_batch['text_encoded_length'].append(item['text_encoded'].shape[1])
+
+        result_batch['spectrogram'].append(item['spectrogram'].squeeze().transpose(0, 1))
+        result_batch['spectrogram_length'].append(item['spectrogram'].shape[2])
+
+        result_batch['text'].append(CharTextEncoder.normalize_text(item['text']))
+
+    result_batch['text_encoded'] = pad_sequence(result_batch['text_encoded'], batch_first=True)
+    result_batch['text_encoded_length'] = torch.Tensor(result_batch['text_encoded_length'])
+    result_batch['spectrogram'] = pad_sequence(result_batch['spectrogram'], batch_first=True)
+
+    return result_batch
--- hw_asr/text_encoder/ctc_char_text_encoder.py
+++ hw_asr/text_encoder/ctc_char_text_encoder.py
@@ -1,4 +1,6 @@
 from typing import List, Tuple
+from itertools import groupby
+from ctcdecode import CTCBeamDecoder
 
 import torch
 
@@ -18,8 +20,12 @@ class CTCCharTextEncoder(CharTextEncoder):
         self.char2ind = {v: k for k, v in self.ind2char.items()}
 
     def ctc_decode(self, inds: List[int]) -> str:
-        # TODO: your code here
-        raise NotImplementedError()
+        inds = [x[0] for x in groupby(inds)]
+        res = ""
+        for ind in inds:
+            if ind:
+                res += self.ind2char[ind]
+        return res
 
     def ctc_beam_search(self, probs: torch.tensor, beam_size: int = 100) -> List[Tuple[str, float]]:
         """
@@ -28,7 +34,13 @@ class CTCCharTextEncoder(CharTextEncoder):
         assert len(probs.shape) == 2
         char_length, voc_size = probs.shape
         assert voc_size == len(self.ind2char)
+
+        decoder = CTCBeamDecoder(list(self.ind2char.values()), beam_width=beam_size)
+        beam_results, beam_scores, _, out_lens = decoder.decode(probs.unsqueeze(0))
         hypos = []
-        # TODO: your code here
-        raise NotImplementedError
-        return sorted(hypos, key=lambda x: x[1], reverse=True)
+        for i in range(beam_size):
+            hypos.append((self.ctc_decode(beam_results[0][i][:out_lens[0][i]].tolist()),
+                          1/torch.exp(beam_scores[0][i]).item()))
+
+        return hypos
+        # return sorted(hypos, key=lambda x: x[1], reverse=True)
